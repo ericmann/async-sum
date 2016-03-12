88ca1695527a88b6780b2bebaffe842ae6939d38
Eric Mann <eric@eamann.com>
2016-03-12 14:59:14 -0800

    Modal-ify the spinner

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,15 @@ jQuery( "#spinner" ).progressbar(
 		value: false
 	}
 );
+var modal = jQuery( '#spinner-wrap' ).dialog(
+	{
+		modal: true,
+		resizable: false,
+		draggable: false,
+		buttons: [],
+		height: 70
+	}
+);
 
 // Set up "localized" data
 window.ASYNC = window.ASYNC || {};
@@ -13,12 +22,11 @@ window.ASYNC.api_base = window.location.href.replace( '/index.html', '' );
 var listings = new window.ASYNC.ListingCollection();
 listings.fetch().done( function() {
 	// Once we've fetched everything, iterate through our collection and render out the associated models
-	var container = jQuery( document.getElementById( 'listing_container' ) ),
-		spinner = jQuery( document.getElementById( 'spinner' ) );
+	var container = jQuery( document.getElementById( 'listing_container' ) );
 
 	_.each( listings.models, function( item ) {
 		container.append( new window.ASYNC.ListingView( { model: item } ).render().$el );
 	} );
 
-	spinner.hide();
+	modal.dialog( 'close' );
 } );
